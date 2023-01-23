49c642a1987b399a26824d1b5bea1854e991cd68
Benjamin D. Brodie <benjamin@bdbrodie.com>
2023-01-23 11:03:27 +0100

    Add model for updating FileScanStatus

---
+++ src/Storage.Interface/Models/FileScanStatus.cs
@@ -0,0 +1,30 @@
+﻿using Altinn.Platform.Storage.Interface.Enums;
+using Newtonsoft.Json;
+
+namespace Altinn.Platform.Storage.Interface.Models
+{
+    /// <summary>
+    /// Represents file scan status for a data element
+    /// </summary>
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class FileScanStatus
+    {
+        /// <summary>
+        /// Gets or sets the MD5 content hash computed by Azure Blob Storage
+        /// </summary>
+        [JsonProperty(PropertyName = "contentHash")]
+        public string ContentHash { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scan result
+        /// </summary>
+        [JsonProperty(PropertyName = "fileScanResult")]
+        public FileScanResult FileScanResult { get; set; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
