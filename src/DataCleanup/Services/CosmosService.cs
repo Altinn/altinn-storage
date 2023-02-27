1f5749dccd5f6a80a014467d733266ff167cb6d7
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-02-27 14:15:10 +0100

    Check if completeconfirmations exists (#169)

--- src/DataCleanup/Services/CosmosService.cs
+++ src/DataCleanup/Services/CosmosService.cs
@@ -234,10 +234,11 @@ namespace Altinn.Platform.Storage.DataCleanup.Services
 
                     foreach (DataElement dataElement in response)
                     {
-                        if (retrievedInstances.ContainsKey(dataElement.InstanceGuid))
+                        if (retrievedInstances.TryGetValue(dataElement.InstanceGuid, out Instance instance))
                         {
-                            if (retrievedInstances[dataElement.InstanceGuid].CompleteConfirmations.Any(
-                                c => c.StakeholderId.ToLower().Equals(retrievedInstances[dataElement.InstanceGuid].Org) && c.ConfirmedOn <= DateTime.UtcNow.AddDays(-7)))
+                            if (instance.CompleteConfirmations != null &&
+                                instance.CompleteConfirmations.Any(c => c.StakeholderId.ToLower().Equals(instance.Org) &&
+                                c.ConfirmedOn <= DateTime.UtcNow.AddDays(-7)))
                             {
                                 dataElements.Add(dataElement);
                             }
@@ -249,14 +250,14 @@ namespace Altinn.Platform.Storage.DataCleanup.Services
                                 .Where(i => i.Id.Equals(dataElement.InstanceGuid));
 
                             var instanceIterator = instanceQuery.ToFeedIterator();
-                            Instance instance = null;
+                            instance = null;
                             while (instanceIterator.HasMoreResults)
                             {
                                 var res = await instanceIterator.ReadNextAsync();
                                 instance = res.FirstOrDefault();
                                 if (instance != null)
-                                { 
-                                    break; 
+                                {
+                                    break;
                                 }
                             }
 
@@ -272,6 +273,7 @@ namespace Altinn.Platform.Storage.DataCleanup.Services
                             else
                             {
                                 retrievedInstances.Add(instance.Id, instance);
+
                                 if (instance.CompleteConfirmations != null &&
                                     instance.CompleteConfirmations.Any(c => c.StakeholderId.ToLower().Equals(instance.Org) &&
                                     c.ConfirmedOn <= DateTime.UtcNow.AddDays(-7)))
