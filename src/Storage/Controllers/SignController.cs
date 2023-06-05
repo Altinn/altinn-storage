e99ffbb2c9781f764ea11471ce82b1163e1e849c
tba76 <thomabak@gmail.com>
2023-06-05 10:53:48 +0200

    New SignController, dummy response

---
+++ src/Storage/Controllers/SignController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Platform.Storage.Controllers
+{
+    /// <summary>
+    /// Handles operations for signing all of or part of dataelements for an instance
+    /// </summary>
+    [Route("storage/api/v1/instances")]
+    [ApiController]
+    public class SignController : ControllerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignController"/> class
+        /// </summary>
+        public SignController()
+        {
+        }
+
+        /// <summary>
+        /// Create signature document from listed data elements
+        /// </summary>
+        /// <param name="instanceOwnerPartyId">The party id of the instance owner.</param>
+        /// <param name="instanceGuid">The id of the instance</param>
+        /// <param name="signRequest">Signrequest containing data element ids and sign status</param>
+        [Authorize(Policy = "Sign")]
+        [HttpPost("{instanceOwnerPartyId:int}/{instanceGuid:guid}/sign")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
+        public async Task<ActionResult> Sign([FromRoute] int instanceOwnerPartyId, [FromRoute] Guid instanceGuid, [FromBody] SignRequest signRequest)
+        {
+            return StatusCode(201, "SignDocument is created");
+        }
+    }
+}
