b5cfa4fbda9f1bfa4e8f4c17fc041c6b5a535ff8
acn-sbuad <stephanie.buadu@avanade.com>
2023-06-07 09:46:40 +0200

    updated models related to signing

--- src/Storage.Interface/Enums/ReferenceType.cs
+++ src/Storage.Interface/Enums/ReferenceType.cs
@@ -1,8 +1,15 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+using TextJson = System.Text.Json.Serialization;
+
 namespace Altinn.Platform.Storage.Interface.Enums;
 
 /// <summary>
 /// The type of the connected object
 /// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+[TextJson.JsonConverter(typeof(TextJson.JsonStringEnumConverter))]
 public enum ReferenceType
 {
     /// <summary>
