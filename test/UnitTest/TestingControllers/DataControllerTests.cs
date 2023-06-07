cc78f224eb213086ea9704634aedef1cdc3e7faa
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2023-06-07 15:50:56 +0200

    Update test/UnitTest/TestingControllers/DataControllerTests.cs

--- test/UnitTest/TestingControllers/DataControllerTests.cs
+++ test/UnitTest/TestingControllers/DataControllerTests.cs
@@ -673,7 +673,7 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
                     }
 
                     services.AddSingleton<IPostConfigureOptions<JwtCookieOptions>, JwtCookiePostConfigureOptionsStub>();
-                    services.AddSingleton<IPublicSigningKeyProvider, PublicSigningKeyProvider>();
+                    services.AddSingleton<IPublicSigningKeyProvider, PublicSigningKeyProviderMock>();
 
                     services.AddSingleton(sasTokenProvider.Object);
                     services.AddSingleton(keyVaultWrapper.Object);
