707537a1b455bf407056bdf686716daa6dc470fd
Henning Normann <h.normann@accenture.com>
2023-08-28 11:38:23 +0200

    Avoid ambigious reference to event - take 3

--- src/Storage/Repository/PgInstanceEventRepository.cs
+++ src/Storage/Repository/PgInstanceEventRepository.cs
@@ -57,7 +57,7 @@ namespace Altinn.Platform.Storage.Repository
 
             await using NpgsqlDataReader reader = await pgcom.ExecuteReaderAsync();
             await reader.ReadAsync();
-            return reader.GetFieldValue<InstanceEvent>("instanceevent");
+            return reader.GetFieldValue<InstanceEvent>("event");
         }
 
         /// <inheritdoc/>
@@ -78,7 +78,7 @@ namespace Altinn.Platform.Storage.Repository
             {
                 while (await reader.ReadAsync())
                 {
-                    events.Add(reader.GetFieldValue<InstanceEvent>("instanceevent"));
+                    events.Add(reader.GetFieldValue<InstanceEvent>("event"));
                 }
             }
 
