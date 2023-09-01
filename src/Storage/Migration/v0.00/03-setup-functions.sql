b8c9599d1f864aacb4ca03bd080210308e2e5887
Henning Normann <h.normann@accenture.com>
2023-09-01 11:11:56 +0200

    Case sensitive filtering of instance event event type

--- src/Storage/Migration/v0.00/03-setup-functions.sql
+++ src/Storage/Migration/v0.00/03-setup-functions.sql
@@ -283,7 +283,7 @@ RETURN QUERY
 	WHERE instance = _instance
 		AND (ie.event->>'Created')::TIMESTAMP >= _from
 		AND (ie.event->>'Created')::TIMESTAMP <= _to
-		AND (_eventtype IS NULL OR ie.event->>'EventType' ILIKE ANY (_eventtype))
+		AND (_eventtype IS NULL OR ie.event->>'EventType' = ANY (_eventtype))
     ORDER BY ie.event->'Created';
 END;
 $BODY$;
