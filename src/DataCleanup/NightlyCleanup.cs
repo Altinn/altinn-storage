70cf5152ed3990f3113921f145b57d1fe8a7c5c6
acn-sbuad <stephanie.buadu@avanade.com>
2023-10-31 10:07:44 +0100

    pass on distinc appids to cosmos

--- src/DataCleanup/NightlyCleanup.cs
+++ src/DataCleanup/NightlyCleanup.cs
@@ -41,10 +41,10 @@ namespace Altinn.Platform.Storage.DataCleanup
         /// <param name="timer">The trigger timer.</param>
         /// <param name="log">The log.</param>
         [FunctionName("NightlyCleanup")]
-        public async Task Run([TimerTrigger("0 0 3 * * *", RunOnStartup = false)] TimerInfo timer, ILogger log)
+        public async Task Run([TimerTrigger("0 0 3 * * *", RunOnStartup = true)] TimerInfo timer, ILogger log)
         {
             List<Instance> instances = await _cosmosService.GetHardDeletedInstances();
-            List<Application> applications = await _cosmosService.GetApplications(instances.Select(i => i.AppId).ToList());
+            List<Application> applications = await _cosmosService.GetApplications(instances.Select(i => i.AppId).Distinct().ToList());
             List<string> autoDeleteAppIds = applications.Where(a => a.AutoDeleteOnProcessEnd == true).Select(a => a.Id).ToList();
             int successfullyDeleted = 0;
 
