938aa16ea006a335518944a6add0a15e5838ceeb
Khan Mohammad R. <codemascot@hotmail.com>
2024-04-10 13:00:34 +0200

    Test added for no party ID but wrong instance owner ID

--- test/UnitTest/TestingControllers/InstancesControllerTests.cs
+++ test/UnitTest/TestingControllers/InstancesControllerTests.cs
@@ -490,6 +490,31 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
         }
 
         /// <summary>
+        /// Test case: Get Multiple instances without specifying instance owner partyId.
+        /// Expected: Returns status bad request.
+        /// </summary>
+        [Fact]
+        public async Task GetMany_UserRequestsInstancesNoPartyIdButWithWrongInstanceOwnerIdDefined_ReturnsBadRequest()
+        {
+            // Arrange
+            string requestUri = $"{BasePath}";
+
+            HttpClient client = GetTestClient();
+            string token = PrincipalUtil.GetToken(3, 1337);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            client.DefaultRequestHeaders.Add("X-Ai-InstanceOwnerIdentifier", "something:3312321321");
+            string expected = "Invalid InstanceOwnerIdentifier.";
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync(requestUri);
+            string responseMessage = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Contains(expected, responseMessage);
+        }
+
+        /// <summary>
         /// Test case: Get Multiple instances and specifying status.isHardDeleted=true.
         /// Expected: No instances included in response.
         /// </summary>
