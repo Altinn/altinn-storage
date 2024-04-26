5eaf60745c14ca01d33285a879786f1e66930741
Daniel Skovli <daniel.skovli@gmail.com>
2024-04-26 10:19:08 +0200

    Uses `CurrentTask.ElementId` instead of property on `SignRequest`

--- src/Storage/Services/InstanceService.cs
+++ src/Storage/Services/InstanceService.cs
@@ -73,7 +73,7 @@ namespace Altinn.Platform.Storage.Services
                 $"{signRequest.SignatureDocumentDataType}.json", 
                 0, 
                 userId.ToString(),
-                null);
+                instance.Process.CurrentTask?.ElementId);
 
             signDocument.Id = dataElement.Id;
         
