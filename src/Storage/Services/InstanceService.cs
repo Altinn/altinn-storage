f64e71beaa5737f037f7f4560cc8f6f42a50afb6
Daniel Skovli <daniel.skovli@gmail.com>
2024-05-13 12:40:17 +0200

    Removes implementation in `CreateSignDocument`

--- src/Storage/Services/InstanceService.cs
+++ src/Storage/Services/InstanceService.cs
@@ -73,7 +73,7 @@ namespace Altinn.Platform.Storage.Services
                 $"{signRequest.SignatureDocumentDataType}.json", 
                 0, 
                 userId.ToString(),
-                signRequest.GeneratedFromTask);
+                null);
 
             signDocument.Id = dataElement.Id;
         
