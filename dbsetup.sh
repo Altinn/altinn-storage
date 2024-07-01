f0e17d7ff9118d0ec93f0f9efb3b80b95c2802ce
acn-sbuad <stephanie.buadu@avanade.com>
2024-07-01 17:05:38 +0200

    moved out of transaction block

--- dbsetup.sh
+++ dbsetup.sh
@@ -1,11 +1,13 @@
  #!/bin/bash
 export PGPASSWORD=Password
 
+# alter max connections
+psql -h localhost -p 5432 -U platform_storage_admin -d storagedb \
+-c "ALTER SYSTEM SET max_connections TO '200';"
+
 # set up platform_storage role
 psql -h localhost -p 5432 -U platform_storage_admin -d storagedb \
 -c "DO \$\$
-    BEGIN
-    ALTER SYSTEM SET max_connections to '200';
-    CREATE ROLE platform_storage WITH LOGIN  PASSWORD 'Password';
+    BEGIN CREATE ROLE platform_storage WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
     END \$\$;"
