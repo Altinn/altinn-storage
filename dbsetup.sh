3a47ce75d8c3d4771ed357d51b1732af249f52b1
acn-sbuad <stephanie.buadu@avanade.com>
2024-07-01 16:53:29 +0200

    set max connections to 200 in setup

--- dbsetup.sh
+++ dbsetup.sh
@@ -4,6 +4,7 @@ export PGPASSWORD=Password
 # set up platform_storage role
 psql -h localhost -p 5432 -U platform_storage_admin -d storagedb \
 -c "DO \$\$
+    ALTER SYSTEM SET max_connections to '200';
     BEGIN CREATE ROLE platform_storage WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
     END \$\$;"
