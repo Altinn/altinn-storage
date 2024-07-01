07363e7f0a21717a2b6379b916a4579c8b2fbbe4
acn-sbuad <stephanie.buadu@avanade.com>
2024-07-01 17:02:51 +0200

    changed order of cmds

--- dbsetup.sh
+++ dbsetup.sh
@@ -4,7 +4,8 @@ export PGPASSWORD=Password
 # set up platform_storage role
 psql -h localhost -p 5432 -U platform_storage_admin -d storagedb \
 -c "DO \$\$
+    BEGIN
     ALTER SYSTEM SET max_connections to '200';
-    BEGIN CREATE ROLE platform_storage WITH LOGIN  PASSWORD 'Password';
+    CREATE ROLE platform_storage WITH LOGIN  PASSWORD 'Password';
     EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
     END \$\$;"
