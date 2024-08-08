21ed9d4c7a6cb149d2690768be685efa001389c2
Henning Normann <h.normann@accenture.com>
2024-08-08 15:17:03 +0200

    Rolled back "Temporary hard code som settings from C# class"

--- src/Storage/Configuration/GeneralSettings.cs
+++ src/Storage/Configuration/GeneralSettings.cs
@@ -51,19 +51,16 @@ namespace Altinn.Platform.Storage.Configuration
         /// <summary>
         /// Gets or sets a semicolon separated white list of IP addresses for the migration controller
         /// </summary>
-        /// TODO: Move hard coding to ops
-        public string MigrationIpWhiteList { get; set; } = "89.250.123.17;89.250.123.58;40.68.3.113;52.234.36.38;10.211.49.188;51.120.0.114";
+        public string MigrationIpWhiteList { get; set; }
 
         /// <summary>
         /// Gets or sets the URI for the ondemand API
         /// </summary>
-        /// TODO: Move hard coding to ops
-        public string OndemandEndpoint { get; set; } = "http://altinn-storage.default.svc.cluster.local/storage/api/v1/";
+        public string OndemandEndpoint { get; set; }
 
         /// <summary>
         /// Gets or sets whether to use Ttd as service owner for environements without "normal" service owners
         /// </summary>
-        /// TODO: Move hard coding to ops
-        public bool A2UseTtdAsServiceOwner { get; set; } = true;
+        public bool A2UseTtdAsServiceOwner { get; set; }
     }
 }
