ef586cecc2fc995b40c9472a4e8b6638d47da806
Johannes Haukland <42615991+HauklandJ@users.noreply.github.com>
2024-08-28 10:49:41 +0200

    Update interface to include sub form prop (#477)
    * add property for allowing a datatype to be used in a sub form
    
    * migration changes
    
    * fix test issue
    
    * Revert "migration changes"
    
    This reverts commit 5ca3c75aced9c77c14fb3e7b68d5b90458f53809.

---
+++ test/Altinn.Platform.Storage.Interface.Tests/AllowInSubform/AllowInSubformTests.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using Altinn.Platform.Storage.Interface.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Altinn.Platform.Storage.Interface.Tests.AllowInSubform;
+
+public class AllowInSubformTests
+{
+    [Fact]
+    public void MetadataWithoutShadowFields_ShouldBeFalse()
+    {
+        Application applicationBefore = TestdataHelper.LoadDataFromEmbeddedResourceAsType<Application>("AllowInSubform.applicationMetadata_beforeChange.json");
+
+        applicationBefore.DataTypes.First(d => d.Id == "Veileder").AppLogic.AllowInSubform.Should().BeFalse();
+    }
+
+    [Fact]
+    public void MetadataWithShadowFields_ShouldBeTrue()
+    {
+        Application applicationBefore = TestdataHelper.LoadDataFromEmbeddedResourceAsType<Application>("AllowInSubform.applicationMetadata_afterChange.json");
+
+        applicationBefore.DataTypes.First(d => d.Id == "Veileder").AppLogic.AllowInSubform.Should().BeTrue();
+    }
+}
