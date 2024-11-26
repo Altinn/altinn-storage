61af79b7986e0abb9e49962b1fd745c301262912
Martin Othamar <martin@othamar.net>
2024-11-26 09:24:37 +0100

    Remove dummy comment

--- src/Storage/Migration/FunctionsAndProcedures/insertinstanceevents.sql
+++ src/Storage/Migration/FunctionsAndProcedures/insertinstanceevents.sql
@@ -3,7 +3,6 @@ CREATE OR REPLACE PROCEDURE storage.insertinstanceevents(_instance UUID, _events
     LANGUAGE 'plpgsql'	
 AS $BODY$
 BEGIN
-	-- Dummy comment to verify that new migration solution works end to end
 	INSERT INTO storage.instanceevents (instance, alternateid, event)
 	SELECT _instance, (evs->>'Id')::UUID, jsonb_strip_nulls(evs) 
 	FROM jsonb_array_elements(_events) evs;
