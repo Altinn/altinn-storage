1da2cab0dbb1e1a0ccc7a7585b6c5cf7a2e5a280
Roar Mjelde <roarmj@gmail.com>
2025-01-23 14:28:38 +0100

    Fixed tests

--- test/UnitTest/TestingControllers/SblBridgeControllerTests.cs
+++ test/UnitTest/TestingControllers/SblBridgeControllerTests.cs
@@ -1,14 +1,19 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using Altinn.Common.AccessToken.Services;
+using Altinn.Common.PEP.Interfaces;
 using Altinn.Platform.Storage.Clients;
 using Altinn.Platform.Storage.Controllers;
 using Altinn.Platform.Storage.UnitTest.Fixture;
+using Altinn.Platform.Storage.UnitTest.Mocks;
+using Altinn.Platform.Storage.UnitTest.Mocks.Authentication;
 using Altinn.Platform.Storage.UnitTest.Mocks.Clients;
 using Altinn.Platform.Storage.UnitTest.Utils;
+using AltinnCore.Authentication.JwtCookie;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace Altinn.Platform.Storage.UnitTest.TestingControllers
@@ -26,10 +31,26 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
         }
 
         [Fact]
-        public async Task RegisterAltinn3CorrespondenceRecipient_InvalidPartyId_ReturnsBadRequestAsync()
+        public async Task RegisterAltinn3CorrespondenceRecipient_ValidPartyId_ReturnsOk()
         {
             var client = GetTestClient();
-            var response = await client.PostAsync("storage/api/v1/sblbridge/correspondencerecipient?partyId=123", null);
+            string token = PrincipalUtil.GetOrgToken("foo", scope: "altinn:correspondence.sblbridge");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.PostAsync("storage/api/v1/sblbridge/correspondencerecipient?partyId=1337", null);
+            var body = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RegisterAltinn3CorrespondenceRecipient_InvalidPartyId_ReturnsBadRequest()
+        {
+            var client = GetTestClient();
+            string token = PrincipalUtil.GetOrgToken("foo", scope: "altinn:correspondence.sblbridge");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.PostAsync("storage/api/v1/sblbridge/correspondencerecipient?partyId=0", null);
+            var body = await response.Content.ReadAsStringAsync();
             Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);           
         }
         
@@ -46,6 +67,8 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
                 builder.ConfigureTestServices(services =>
                 {
                     services.AddSingleton<IPartiesWithInstancesClient, PartiesWithInstancesClientMock>();
+                    services.AddSingleton<IPublicSigningKeyProvider, PublicSigningKeyProviderMock>();
+                    services.AddSingleton<IPostConfigureOptions<JwtCookieOptions>, JwtCookiePostConfigureOptionsStub>();
                 });
             }).CreateClient();
 
