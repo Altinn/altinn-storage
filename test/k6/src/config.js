868e23f975dc7c7e772b53888b55c7a2af54bb08
Terje Holene <terje.holene@gmail.com>
2025-04-14 14:45:35 +0200

    Set correct auth cookie name for altinnii

--- test/k6/src/config.js
+++ test/k6/src/config.js
@@ -14,10 +14,23 @@ var maskinportenBaseUrls = {
   prod: "https://maskinporten.no/",
 };
 
+// Auth cookie names in the different environments. NB: Must be updated until changes
+// are rolled out to all environments
+export var authCookieNames = {
+  at21: '.ASPXAUTH', // '.AspxAuthCloud',
+  at22: '.AspxAuthCloud',
+  at23: '.ASPXAUTH', // '.AspxAuthCloud',
+  at24: '.AspxAuthCloud',
+  tt02: '.AspxAuthTT02',
+  yt01: '.ASPXAUTH', // '.AspxAuthYt',
+  prod: '.ASPXAUTH', // '.AspxAuthProd'
+};
 
 //Get values from environment
 const environment = __ENV.env.toLowerCase();
 export let baseUrl = baseUrls[environment];
+export let authCookieName = authCookieNames[environment];
+
 let maskinportenBaseUrl = maskinportenBaseUrls[environment];
 
 //Altinn API
