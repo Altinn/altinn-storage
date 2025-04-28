419da994445571ed2b28549c49963e565c7536e9
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-04-28 15:52:42 +0200

    Add System.Text.Json's property name attribute in addition to newtonsoft's.

--- src/Storage.Interface/Models/Signee.cs
+++ src/Storage.Interface/Models/Signee.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable 
 
 using System;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace Altinn.Platform.Storage.Interface.Models
@@ -14,24 +15,28 @@ namespace Altinn.Platform.Storage.Interface.Models
         /// The userId representing the signee.
         /// </summary>
         [JsonProperty(PropertyName = "userId")]
+        [JsonPropertyName("userId")]
         public string? UserId { get; set; }
 
         /// <summary>
         /// The ID of the systemuser performing the signing
         /// </summary>
         [JsonProperty(PropertyName = "systemUserId")]
+        [JsonPropertyName("systemUserId")]
         public Guid? SystemUserId { get; set; }
 
         /// <summary>
         /// The personNumber representing the signee.
         /// </summary>
         [JsonProperty(PropertyName = "personNumber")]
+        [JsonPropertyName("personNumber")]
         public string? PersonNumber { get; set; }
 
         /// <summary>
         /// The organisationNumber representing the signee.
         /// </summary>
         [JsonProperty(PropertyName = "organisationNumber")]
+        [JsonPropertyName("organisationNumber")]
         public string? OrganisationNumber { get; set; }
     }
 }
