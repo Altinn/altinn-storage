a14e5b75023335d264369d3181e7c26cfcc6fb4b
Bjørn Tore Gjerde <bjorn-tore.gjerde@digdir.no>
2025-04-28 16:07:20 +0200

    Feature/sign doc system text json support (#696)
    * Add System.Text.Json's property name attribute in addition to newtonsoft's.
    
    * In signing service, use web defaults for json serializing, and use indentation.

--- src/Storage.Interface/Models/SignDocument.cs
+++ src/Storage.Interface/Models/SignDocument.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 #nullable enable
@@ -14,30 +15,35 @@ namespace Altinn.Platform.Storage.Interface.Models
         /// Unique id of the SignDocument (identical to dataElementId for this document).
         /// </summary>
         [JsonProperty(PropertyName = "id")]
+        [JsonPropertyName("id")]
         public string Id { get; set; } = string.Empty;
 
         /// <summary>
         /// Instance guid.
         /// </summary>
         [JsonProperty(PropertyName = "instanceGuid")]
+        [JsonPropertyName("instanceGuid")]
         public string InstanceGuid { get; set; } = string.Empty;  
 
         /// <summary>
         /// Timestamp for when the document was signed.
         /// </summary>
         [JsonProperty(PropertyName = "signedTime")]
+        [JsonPropertyName("signedTime")]
         public DateTime SignedTime { get; set; }
         
         /// <summary>
         /// Information about the signee.
         /// </summary>
         [JsonProperty(PropertyName = "signeeInfo")]
+        [JsonPropertyName("signeeInfo")]
         public Signee SigneeInfo { get; set; } = new Signee();
 
         /// <summary>
         /// List of dataElementSignatures.
         /// </summary>
         [JsonProperty(PropertyName = "dataElementSignatures")]
+        [JsonPropertyName("dataElementSignatures")]
         public List<DataElementSignature> DataElementSignatures { get; set; } = new List<DataElementSignature>();
         
         /// <summary>
@@ -49,18 +55,21 @@ namespace Altinn.Platform.Storage.Interface.Models
             /// Id of the dataElement.
             /// </summary>
             [JsonProperty(PropertyName = "dataElementId")]
+            [JsonPropertyName("dataElementId")]
             public string DataElementId { get; set; } = string.Empty;
 
             /// <summary>
             /// Sha256 hash of the dataelement.
             /// </summary>
             [JsonProperty(PropertyName = "sha256Hash")]
+            [JsonPropertyName("sha256Hash")]
             public string Sha256Hash { get; set; } = string.Empty;
 
             /// <summary>
             /// Signing status for dataElement.
             /// </summary>
             [JsonProperty(PropertyName = "signed")]
+            [JsonPropertyName("signed")]
             public bool Signed { get; set; }
         }
     }
