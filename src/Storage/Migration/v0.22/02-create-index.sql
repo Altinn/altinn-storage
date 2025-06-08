c7279ffa5ddf206b7a85fdabbc40478d497c4471
Henning Normann <h.normann@accenture.com>
2025-06-08 20:58:57 +0200

    Rolled back concurrently

--- src/Storage/Migration/v0.22/02-create-index.sql
+++ src/Storage/Migration/v0.22/02-create-index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS instances_lastchanged_org_not_confirmed_by_org ON storage.instances(org, lastchanged desc, id) WHERE confirmed = False;
+CREATE INDEX IF NOT EXISTS instances_lastchanged_org_not_confirmed_by_org ON storage.instances(org, lastchanged desc, id) WHERE confirmed = False;
