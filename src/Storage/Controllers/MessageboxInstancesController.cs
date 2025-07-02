590f7428d4d6e567a1d935c8eaa7f8fd7b3567f1
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-07-02 15:26:54 +0200

    Log {partyId}/{guid} similarly to the other Wolverine commanders

--- src/Storage/Controllers/MessageboxInstancesController.cs
+++ src/Storage/Controllers/MessageboxInstancesController.cs
@@ -412,7 +412,7 @@ namespace Altinn.Platform.Storage.Controllers
                 catch (Exception ex)
                 {
                     // Log the error but do not return an error to the user
-                    _logger.LogError(ex, "Failed to publish instance update command for instance {InstanceId}", instanceGuid.ToString());
+                    _logger.LogError(ex, "Failed to publish instance update command for instance {InstanceId}", instanceId);
                 }
             }
             
