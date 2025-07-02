c7fe5fd1e7ecb27760567d5794ef143435e5e59c
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-07-02 12:47:21 +0200

    Set all props as required in command record

--- src/Storage/Messages/SyncInstanceToDialogportenCommand.cs
+++ src/Storage/Messages/SyncInstanceToDialogportenCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Wolverine.Attributes;
 
 namespace Altinn.Platform.Storage.Messages;
@@ -7,8 +8,9 @@ namespace Altinn.Platform.Storage.Messages;
 /// Represents a message about update for an instance to send to service bus.
 /// </summary>
 [MessageIdentity("Altinn.DialogportenAdapter.SyncInstanceCommand")]
-public record SyncInstanceToDialogportenCommand(string AppId, // eks: krt/krt-1012a-1
-    string PartyId, // eks: 51701090
-    string InstanceId, // eks: 0dbc1da6-f744-4fff-83bc-131e7988a1bb
-    DateTime InstanceCreatedAt, // eks: 2025-06-18T08:54:53.6233769Z
-    bool IsMigration); // Alltid false fra dere
+public record SyncInstanceToDialogportenCommand(
+    [Required] string AppId, // eks: krt/krt-1012a-1
+    [Required] string PartyId, // eks: 51701090
+    [Required] string InstanceId, // eks: 0dbc1da6-f744-4fff-83bc-131e7988a1bb
+    [Required] DateTime InstanceCreatedAt, // eks: 2025-06-18T08:54:53.6233769Z
+    [Required] bool IsMigration); // Always false when Storage is sender
