5272233dc26d0d0f08bdcebd9624f41dde25ed1d
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-07-02 16:01:35 +0200

    Revert the requiredattribute annotations

--- src/Storage/Messages/SyncInstanceToDialogportenCommand.cs
+++ src/Storage/Messages/SyncInstanceToDialogportenCommand.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using Wolverine.Attributes;
 
 namespace Altinn.Platform.Storage.Messages;
@@ -9,8 +8,8 @@ namespace Altinn.Platform.Storage.Messages;
 /// </summary>
 [MessageIdentity("Altinn.DialogportenAdapter.SyncInstanceCommand")]
 public record SyncInstanceToDialogportenCommand(
-    [Required] string AppId, // eks: krt/krt-1012a-1
-    [Required] string PartyId, // eks: 51701090
-    [Required] string InstanceId, // eks: 0dbc1da6-f744-4fff-83bc-131e7988a1bb
-    [Required] DateTime InstanceCreatedAt, // eks: 2025-06-18T08:54:53.6233769Z
-    [Required] bool IsMigration); // Always false when Storage is sender
+    string AppId, // eks: krt/krt-1012a-1
+    string PartyId, // eks: 51701090
+    string InstanceId, // eks: 0dbc1da6-f744-4fff-83bc-131e7988a1bb
+    DateTime InstanceCreatedAt, // eks: 2025-06-18T08:54:53.6233769Z
+    bool IsMigration); // Always false when Storage is sender
