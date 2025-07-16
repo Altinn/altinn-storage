5d5e5eed5551eb13fe61e55d6ba4deb410874540
Daniel Skovli <daniel.skovli@gmail.com>
2025-07-16 12:45:39 +0200

    Adds read/write protection for sensitive data elements in DataController

---
+++ src/Storage/Extensions/DataTypeExtensions.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Altinn.Platform.Storage.Authorization;
+using Altinn.Platform.Storage.Interface.Models;
+
+namespace Altinn.Platform.Storage.Extensions;
+
+/// <summary>
+/// Utility methods for DataType objects.
+/// </summary>
+internal static class DataTypeExtensions
+{
+    /// <summary>
+    /// Checks if the user has permission to read data of this type for the given instance.
+    /// </summary>
+    public static async Task<bool> CanRead(
+        this DataType dataType,
+        IAuthorization authorizationService,
+        Instance instance,
+        string task = null)
+    {
+        if (string.IsNullOrWhiteSpace(dataType.ActionRequiredToRead))
+        {
+            return true;
+        }
+
+        return await authorizationService.AuthorizeInstanceAction(
+            instance,
+            dataType.ActionRequiredToRead,
+            task ?? instance.Process?.CurrentTask?.ElementId);
+    }
+
+    /// <summary>
+    /// Checks if the user has permission to write data of this type for the given instance.
+    /// </summary>
+    public static async Task<bool> CanWrite(
+        this DataType dataType,
+        IAuthorization authorizationService,
+        Instance instance,
+        string task = null)
+    {
+        if (string.IsNullOrWhiteSpace(dataType.ActionRequiredToWrite))
+        {
+            return true;
+        }
+
+        return await authorizationService.AuthorizeInstanceAction(
+            instance,
+            dataType.ActionRequiredToWrite,
+            task ?? instance.Process?.CurrentTask?.ElementId);
+    }
+}
