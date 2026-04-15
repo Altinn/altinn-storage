5330778da39fa31981909263721a447b057b270a
Konrad-Simso <konrad.simso@digdir.no>
2026-04-15 14:37:10 +0200

    feat: update index to not be created concurrently, tests fail.

--- src/Storage/Migration/v0.30/01-create-index.sql
+++ src/Storage/Migration/v0.30/01-create-index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS instances_org_lastchanged_id_filtered_by_ended
+CREATE INDEX IF NOT EXISTS instances_org_lastchanged_id_filtered_by_ended
 ON storage.instances USING btree
 (org COLLATE pg_catalog."default" ASC NULLS LAST, lastchanged DESC NULLS FIRST, id ASC NULLS LAST)
 TABLESPACE pg_default
