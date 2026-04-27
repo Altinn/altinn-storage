b6dffe19cf5a8c043c6b77a0978c60cdc24a24c8
Johannes Haukland <42615991+HauklandJ@users.noreply.github.com>
2026-04-27 09:12:56 +0200

    chore: file scoped ns (#966)

--- src/Storage.Interface/Models/DataField.cs
+++ src/Storage.Interface/Models/DataField.cs
@@ -2,29 +2,28 @@
 
 using System.Text.Json.Serialization;
 
-namespace Altinn.Platform.Storage.Interface.Models
+namespace Altinn.Platform.Storage.Interface.Models;
+
+/// <summary>
+/// Holds the definition of a data field for an application, ie. a named reference to a form data field.
+/// </summary>
+public class DataField
 {
     /// <summary>
-    /// Holds the definition of a data field for an application, ie. a named reference to a form data field.
+    /// Gets or sets the id of the presentation field
     /// </summary>
-    public class DataField
-    {
-        /// <summary>
-        /// Gets or sets the id of the presentation field
-        /// </summary>
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
 
-        /// <summary>
-        /// Gets or sets the path of the presentation field
-        /// </summary>
-        [JsonPropertyName("path")]
-        public string Path { get; set; }
+    /// <summary>
+    /// Gets or sets the path of the presentation field
+    /// </summary>
+    [JsonPropertyName("path")]
+    public string Path { get; set; }
 
-        /// <summary>
-        /// Gets or sets the data type where the presentation field is defined.
-        /// </summary>
-        [JsonPropertyName("dataTypeId")]
-        public string DataTypeId { get; set; }
-    }
+    /// <summary>
+    /// Gets or sets the data type where the presentation field is defined.
+    /// </summary>
+    [JsonPropertyName("dataTypeId")]
+    public string DataTypeId { get; set; }
 }
