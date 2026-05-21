2ab3df21ea1dbec01d9cc030859cb48b13bdb212
Konrad-Simso <konrad.simso@digdir.no>
2026-05-21 14:44:44 +0200

    fix: docs

--- src/Storage/Models/Metrics/DailyMetrics.cs
+++ src/Storage/Models/Metrics/DailyMetrics.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace Altinn.Platform.Storage.Models.Metrics;
 
 /// <summary>
-/// Model for metrics for daily statistics for either email or sms
+/// Generic container for a day's worth of metrics records of type <typeparamref name="T"/>.
 /// </summary>
 public record DailyMetrics<T>
 {
@@ -23,7 +23,7 @@ public record DailyMetrics<T>
     public int Year { get; init; }
 
     /// <summary>
-    /// A list of metrics for each individual notification
+    /// A list of metrics
     /// </summary>
     public List<T> Metrics { get; init; } = [];
 }
