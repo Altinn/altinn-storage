b0a957492364bdd7993ebc5808e34fc02c749cc3
Konrad-Simso <konrad.simso@digdir.no>
2026-05-28 16:03:14 +0200

    fix: remove concurrently from index due to test failing.

--- src/Storage/Migration/v0.31/01-create-index.sql
+++ src/Storage/Migration/v0.31/01-create-index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS instances_archived_appid_id_filtered
+CREATE INDEX IF NOT EXISTS instances_archived_appid_id_filtered
 ON storage.instances USING btree
 (
     jsonb_text_to_timestamptz(instance, 'Status', 'Archived') DESC NULLS LAST,
