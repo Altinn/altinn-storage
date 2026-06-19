bdf2236f94603c14836cc96111650ba88b667351
Konrad-Simso <konrad.simso@digdir.no>
2026-06-19 09:30:19 +0200

    feat: update index.

--- src/Storage/Migration/v0.32/01-create-index.sql
+++ src/Storage/Migration/v0.32/01-create-index.sql
@@ -2,4 +2,4 @@ CREATE INDEX IF NOT EXISTS instances_a2archref
 ON storage.instances USING btree
 ((instance -> 'DataValues' ->> 'A2ArchRef') COLLATE pg_catalog."default" ASC NULLS LAST)
 TABLESPACE pg_default
-WHERE (instance -> 'DataValues' ->> 'A2ArchRef') IS NOT NULL and altinnmainversion = 2;
+WHERE altinnmainversion = 2;
