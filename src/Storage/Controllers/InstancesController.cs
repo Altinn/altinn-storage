d8f8dacbd22d0150856eeb9663ae5fe4a7d21ae3
Konrad-Simso <konrad.simso@digdir.no>
2026-06-25 08:25:34 +0200

    fix: remove unnecessary null check

--- src/Storage/Controllers/InstancesController.cs
+++ src/Storage/Controllers/InstancesController.cs
@@ -285,10 +285,7 @@ public class InstancesController : ControllerBase
         if (!string.IsNullOrEmpty(queryParameters.DataValuesA2ArchRef))
         {
             queryParameters.MainVersionInclude = 2;
-            if (queryParameters.MainVersionExclude is not null)
-            {
-                queryParameters.MainVersionExclude = null;
-            }
+            queryParameters.MainVersionExclude = null;
         }
         // Default is to exclude migrated altinn 1 and 2 instances
         else if (
