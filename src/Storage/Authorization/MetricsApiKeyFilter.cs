9f97c00cd813a84c435b9d2a18d6ac7c047add53
Konrad-Simso <konrad.simso@digdir.no>
2026-06-26 14:58:00 +0200

    fix: remove unused using directive

--- src/Storage/Authorization/MetricsApiKeyFilter.cs
+++ src/Storage/Authorization/MetricsApiKeyFilter.cs
@@ -1,7 +1,6 @@
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
