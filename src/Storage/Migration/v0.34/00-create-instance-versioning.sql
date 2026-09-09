602bfd16112023a829ec7606b0d1349f556973ec
Konrad-Simso <konrad.simso@digdir.no>
2026-09-09 09:39:16 +0200

    fix: migration script, inline function for if check (#1089)

--- src/Storage/Migration/v0.34/00-create-instance-versioning.sql
+++ src/Storage/Migration/v0.34/00-create-instance-versioning.sql
@@ -1,5 +1,22 @@
-ALTER TABLE storage.instances
-ADD COLUMN IF NOT EXISTS instance_version INT NOT NULL DEFAULT 1;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_attribute
+        WHERE attrelid = 'storage.instances'::regclass
+          AND attname = 'instance_version'
+          AND attnum > 0 AND NOT attisdropped
+    ) THEN
+        ALTER TABLE storage.instances
+        ADD COLUMN IF NOT EXISTS instance_version INT NOT NULL DEFAULT 1;
+    END IF;
 
-ALTER TABLE storage.instances
-ADD COLUMN IF NOT EXISTS process_state_version INT NOT NULL DEFAULT 1;
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_attribute
+        WHERE attrelid = 'storage.instances'::regclass
+          AND attname = 'process_state_version'
+          AND attnum > 0 AND NOT attisdropped
+    ) THEN
+        ALTER TABLE storage.instances
+        ADD COLUMN IF NOT EXISTS process_state_version INT NOT NULL DEFAULT 1;
+    END IF;
+END $$;
